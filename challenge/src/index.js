1a6a4591cd7334d66d36ab02f90203d231c79df1
Sasanth Tammareddi <stammareddi@nbp-249-35.nbp.ruw.rutgers.edu>
2020-01-23 17:20:59 -0500

    CounterTracker

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -1,15 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { directive } from '@babel/types';
 
-function main(useServiceWorker) {
-    ReactDOM.render(<App />, document.getElementById("root"));
-    if (useServiceWorker) {
-        serviceWorker.register();
-    } else {
-        serviceWorker.unregister();
+
+class Counter extends React.Component{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            count: 0
+        };
+       //this.handleIncrement = this.handleIncrement.bind(this)
+    }
+
+      handleIncrement = () =>
+      { 
+          alert('This webpage is used to track the number of times this message has been viewed');
+          this.setState({ count: this.state.count +1});
+          
+      }
+
+     
+     render()
+    {
+        return (
+            <div align = 'center' >
+                
+                <div>{this.state.count}</div>
+                
+                <div>
+                  <button
+                    onClick = {
+                        this.handleIncrement
+                    } 
+                    
+                   >Click me for more info!</button>
+                </div>
+            </div>
+        )
     }
 }
+ReactDOM.render(
+    <Counter/>,
+    document.querySelector('#root')
+)
 
-main(false);
+ 
